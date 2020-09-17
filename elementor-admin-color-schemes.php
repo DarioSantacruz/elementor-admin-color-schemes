d08b2fe47ac5deb9cd72d660fb5ac4ddc0572fac
Dario Santacruz <mail@dariosantacruz.com>
2020-09-17 06:12:02 -0700

    Color combinations added to the profiles

--- elementor-admin-color-schemes.php
+++ elementor-admin-color-schemes.php
@@ -39,6 +39,7 @@ const VERSION = '0.0.1';
  *
  * @param string $color The folder name for this color scheme.
  */
+
 function get_color_url( $color ) {
 	$suffix = is_rtl() ? '-rtl' : '';
 	return plugins_url( "$color/colors$suffix.css?v=" . VERSION, __FILE__ );
@@ -49,17 +50,75 @@ function get_color_url( $color ) {
  */
 function add_colors() {
 	wp_admin_css_color(
-		'elementor',
+		'elementor_blue',
 		__( 'Elementor Blue', 'admin_schemes' ),
 		get_color_url( 'elementor-blue' ),
-		array( '#010038', '#46F2B6', '#5ef2be' ),
+		array( '#010038', '#46F2B6' ),
 		array(
 			'base' => '#F0F2F1',
 			'focus' => '#fff',
-			'current' => '#f00',
+			'current' => '#fff',
 		)
 	);
-	
+
+	wp_admin_css_color(
+		'elementor_green',
+		__( 'Elementor Green', 'admin_schemes' ),
+		get_color_url( 'elementor-green' ),
+		array( '#46F2B6', '#010038' ),
+		array(
+			'base' => '#F0F2F1',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+	wp_admin_css_color(
+		'elementor_creame',
+		__( 'Elementor Creame', 'admin_schemes' ),
+		get_color_url( 'elementor-creame' ),
+		array( '#FFBC7D', '#FF3700' ),
+		array(
+			'base' => '#F0F2F1',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+	wp_admin_css_color(
+		'elementor_orange',
+		__( 'Elementor Orange', 'admin_schemes' ),
+		get_color_url( 'elementor-orange' ),
+		array( '#FF3700', '#FFBC7D' ),
+		array(
+			'base' => '#F0F2F1',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+	wp_admin_css_color(
+		'elementor_purple',
+		__( 'Elementor Purple', 'admin_schemes' ),
+		get_color_url( 'elementor-purple' ),
+		array( '#92003B', '#FF7BE5'),
+		array(
+			'base' => '#F0F2F1',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+	wp_admin_css_color(
+		'elementor_pink	',
+		__( 'Elementor Pink', 'admin_schemes' ),
+		get_color_url( 'elementor-pink' ),
+		array( '#FF7BE5', '#92003B'),
+		array(
+			'base' => '#F0F2F1',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
 }
 
 add_action( 'admin_init', __NAMESPACE__ . '\add_colors' );
